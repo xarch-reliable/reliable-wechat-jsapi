1b6c361913628f16c634adeea9b057984773f49a
wwq <wwq@localhost.com>
2019-06-08 15:35:47 +0800

    Update PayIdCacheServerImpl.java

--- reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/service/impl/PayIdCacheServerImpl.java
+++ reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/service/impl/PayIdCacheServerImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.xarch.reliable.service.PayIdCacheServer;
 
 @Service
-public class ActidCacheServerImpl implements PayIdCacheServer {
+public class PayIdCacheServerImpl implements PayIdCacheServer {
 
 	@Override
 	public Map<String, String> setActidMap(String actid, Map<String, String> map) {
