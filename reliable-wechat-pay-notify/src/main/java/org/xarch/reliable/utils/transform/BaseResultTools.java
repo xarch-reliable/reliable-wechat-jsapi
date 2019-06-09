850e8fb4f20f8f91550093b07cef72cb1781f29e
wwq <wwq@localhost.com>
2019-06-09 16:11:19 +0800

    修正待测

--- reliable-wechat-pay-notify/src/main/java/org/xarch/reliable/utils/transform/BaseResultTools.java
+++ reliable-wechat-pay-notify/src/main/java/org/xarch/reliable/utils/transform/BaseResultTools.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
@@ -19,20 +20,10 @@ public class BaseResultTools {
 	@SuppressWarnings("unchecked")
 	public static <T> T fromXML(String xmlString, Class<T> clz) {
 		try {
+			xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 			return xmlMapper.readValue(xmlString, clz);
 		} catch (IOException e) {
 			logger.error("[xml]-->[pojo]失败：" + xmlString);
-			e.printStackTrace();
-		}
-		return (T) new Object();
-	}
-
-	@SuppressWarnings("unchecked")
-	public static <T> T fromJSON(String jsonlString, Class<T> clz) {
-		try {
-			return oMapper.readValue(jsonlString, clz);
-		} catch (Exception e) {
-			logger.error("[xml]-->[pojo]失败：" + jsonlString);
 			e.printStackTrace();
 		}
 		return (T) new Object();
@@ -43,25 +34,46 @@ public class BaseResultTools {
 		return xmlMapper.convertValue(object, Map.class);
 	}
 
-	public static <T> T MapToObject(Map<String, Object> map, Class<T> clz) {
-		return oMapper.convertValue(map, clz);
-	}
-
-	public static String JsonObjectToStr(Object object) {
-		try {
-			return oMapper.writeValueAsString(object);
-		} catch (JsonProcessingException e) {
-			logger.error("[pojo]-->[JsonString]序列化失败");
-			e.printStackTrace();
-		}
-		return new String();
-	}
-
 	public static String XmlObjectToStr(Object object) {
 		try {
 			return xmlMapper.writeValueAsString(object);
 		} catch (JsonProcessingException e) {
 			logger.error("[pojo]-->[XmlString]序列化失败");
+			e.printStackTrace();
+		}
+		return new String();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T fromJSON(String jsonlString, Class<T> clz) {
+		try {
+			oMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+			return oMapper.readValue(jsonlString, clz);
+		} catch (Exception e) {
+			logger.error("[string]-->[pojo]失败：" + jsonlString);
+			e.printStackTrace();
+		}
+		return (T) new Object();
+	}
+
+	public static <T> T MapToObject(Map<String, Object> map, Class<T> clz) {
+		oMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		return oMapper.convertValue(map, clz);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Map<String, Object> ObjectToMap(Object object) {
+		oMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		// oMapper.setSerializationInclusion(Include.NON_NULL);
+		return oMapper.convertValue(object, Map.class);
+	}
+
+	public static String JsonObjectToStr(Object object) {
+		try {
+			// oMapper.setSerializationInclusion(Include.NON_NULL);
+			return oMapper.writeValueAsString(object);
+		} catch (JsonProcessingException e) {
+			logger.error("[pojo]-->[JsonString]序列化失败");
 			e.printStackTrace();
 		}
 		return new String();
--- reliable-wechat-pay/src/main/java/org/xarch/reliable/controller/PayController.java
+++ reliable-wechat-pay/src/main/java/org/xarch/reliable/controller/PayController.java
@@ -1,23 +1,25 @@
 package org.xarch.reliable.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.xarch.reliable.model.domain.result.WxPayRefundResult;
-import org.xarch.reliable.model.domain.result.h5.WxPayMpOrderResult;
 import org.xarch.reliable.service.WxPayService;
+import org.xarch.reliable.utils.transform.BaseResultTools;
 
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/wechat")
 public class PayController {
 
 	@Autowired
 	private WxPayService wxPayService;
 
 	@RequestMapping("/pay/h5")
-	public Mono<WxPayMpOrderResult> PayMpOrder(@RequestParam(value = "openid", required = true) String openid,
+	public Mono<Map<String, Object>> PayMpOrder(@RequestParam(value = "openid", required = true) String openid,
 			@RequestParam(value = "payid", required = true) String payid) {
 		String body = "测试";
 		String detail = "保证红包";
@@ -25,12 +27,17 @@ public class PayController {
 		String totalFee = "1";
 		String outTradeNo = payid;
 		String ip = "127.0.0.1";
-		return wxPayService.testCreateOrder_jsapi(body, outTradeNo, totalFee, ip, openid, detail, attach);
+		return wxPayService.testCreateOrder_jsapi(body, outTradeNo, totalFee, ip, openid, detail, attach)
+				.flatMap(res -> {
+					return Mono.just(BaseResultTools.ObjectToMap(res));
+				});
 	}
 
 	@RequestMapping("/pay/refund")
-	public Mono<WxPayRefundResult> PayRefund(@RequestParam(value = "payid", required = true) String payid) {
+	public Mono<Map<String, Object>> PayRefund(@RequestParam(value = "payid", required = true) String payid) {
 		String outRefundNo = String.valueOf(System.currentTimeMillis());
-		return wxPayService.refund(outRefundNo, payid);
+		return wxPayService.refund(outRefundNo, payid).flatMap(res -> {
+			return Mono.just(BaseResultTools.ObjectToMap(res));
+		});
 	}
 }
