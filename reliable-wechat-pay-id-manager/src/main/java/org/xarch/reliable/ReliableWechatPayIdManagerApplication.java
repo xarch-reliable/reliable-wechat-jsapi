3313c00e4f77070b3fe18054c9d228d76383c84a
wancy <wancy@wancy.com>
2019-06-26 00:10:48 +0800

    修正

--- reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/ReliableWechatPayIdManagerApplication.java
+++ reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/ReliableWechatPayIdManagerApplication.java
@@ -2,8 +2,10 @@ package org.xarch.reliable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ReliableWechatPayIdManagerApplication {
 
 	public static void main(String[] args) {
