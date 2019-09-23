ecc48aecded00cb647e76c29b1344fb620749068
wancy <wancy@wancy.com>
2019-09-23 14:54:15 +0800

    订单号

--- reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/service/impl/PayIdManagerServerImpl.java
+++ reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/service/impl/PayIdManagerServerImpl.java
@@ -1,7 +1,5 @@
 package org.xarch.reliable.service.impl;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -15,23 +13,18 @@ import org.xarch.reliable.service.PayIdManagerServer;
 
 @Service
 public class PayIdManagerServerImpl implements PayIdManagerServer {
-	
-	private static final Logger logger = LoggerFactory.getLogger(PayIdManagerServerImpl.class);
 
+	private static final Logger logger = LoggerFactory.getLogger(PayIdManagerServerImpl.class);
+	
 	@Autowired
 	private PayIdCacheServer payIdCacheServer;
 
 	@Override
 	public Map<String, String> addActid2Openid(String actid, String openid) {
 		Map<String, String> map = payIdCacheServer.getActidMap(actid, new HashMap<String, String>());
-		
-		SimpleDateFormat simpleDateFormat;
-		simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-		Date date = new Date();
-		String str = simpleDateFormat.format(date);
 		Random random = new Random();
 		int rannum = (int) (random.nextDouble() * (99999 - 10000 + 1)) + 10000;// 获取5位随机数
-		String outTradeNo = str+rannum;
+		String outTradeNo = String.valueOf(System.currentTimeMillis()) + rannum;
 		logger.info("[outTradeNo]"+outTradeNo);
 		map.put(openid, outTradeNo);
 		return payIdCacheServer.setActidMap(actid, map);
