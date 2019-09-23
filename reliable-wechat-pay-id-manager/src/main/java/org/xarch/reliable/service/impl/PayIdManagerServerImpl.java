44a436ac1c625240aa47d27b860d63bd7dc733e5
wancy <wancy@wancy.com>
2019-09-23 13:58:36 +0800

    test

--- reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/service/impl/PayIdManagerServerImpl.java
+++ reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/service/impl/PayIdManagerServerImpl.java
@@ -3,6 +3,8 @@ package org.xarch.reliable.service.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xarch.reliable.service.PayIdCacheServer;
@@ -10,6 +12,8 @@ import org.xarch.reliable.service.PayIdManagerServer;
 
 @Service
 public class PayIdManagerServerImpl implements PayIdManagerServer {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PayIdManagerServerImpl.class);
 
 	@Autowired
 	private PayIdCacheServer payIdCacheServer;
@@ -18,6 +22,7 @@ public class PayIdManagerServerImpl implements PayIdManagerServer {
 	public Map<String, String> addActid2Openid(String actid, String openid) {
 		Map<String, String> map = payIdCacheServer.getActidMap(actid, new HashMap<String, String>());
 		String outTradeNo = String.valueOf(System.currentTimeMillis()+(int)((Math.random()*9+1)*100000));
+		logger.info("[outTradeNo]"+outTradeNo);
 		map.put(openid, outTradeNo);
 		return payIdCacheServer.setActidMap(actid, map);
 	}
