913267679a1347fe1bbdf71e580f73d772108d9a
wancy <wancy@wancy.com>
2019-09-25 12:46:00 +0800

    Revert "修改payid返回值"
    This reverts commit 9e837f3e190d71a1d7aaf8ead586a57d7f8a9b0a.

--- reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/service/impl/PayIdManagerServerImpl.java
+++ reliable-wechat-pay-id-manager/src/main/java/org/xarch/reliable/service/impl/PayIdManagerServerImpl.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xarch.reliable.service.PayIdCacheServer;
@@ -11,26 +13,21 @@ import org.xarch.reliable.service.PayIdManagerServer;
 
 @Service
 public class PayIdManagerServerImpl implements PayIdManagerServer {
+
+	private static final Logger logger = LoggerFactory.getLogger(PayIdManagerServerImpl.class);
 	
 	@Autowired
 	private PayIdCacheServer payIdCacheServer;
 
 	@Override
 	public Map<String, String> addActid2Openid(String actid, String openid) {
-		Map<String, String> resmap = new HashMap<String, String>();
-		Map<String, String> payidmap = payIdCacheServer.getActidMap(actid, new HashMap<String, String>());
-		String addMsg = payidmap.get(openid);
-		if(addMsg == null) {
-			Random random = new Random();
-			int rannum = (int) (random.nextDouble() * (99999 - 10000 + 1)) + 10000;// 获取5位随机数
-			String outTradeNo = String.valueOf(System.currentTimeMillis()) + rannum;
-			payidmap.put(openid, outTradeNo);
-			payIdCacheServer.setActidMap(actid, payidmap);
-			resmap.put(openid, outTradeNo);
-		}else {
-			resmap.put("error_msg", "false");
-		}
-		return resmap;
+		Map<String, String> map = payIdCacheServer.getActidMap(actid, new HashMap<String, String>());
+		Random random = new Random();
+		int rannum = (int) (random.nextDouble() * (99999 - 10000 + 1)) + 10000;// 获取5位随机数
+		String outTradeNo = String.valueOf(System.currentTimeMillis()) + rannum;
+		logger.info("[outTradeNo]"+outTradeNo);
+		map.put(openid, outTradeNo);
+		return payIdCacheServer.setActidMap(actid, map);
 	}
 
 	@Override
