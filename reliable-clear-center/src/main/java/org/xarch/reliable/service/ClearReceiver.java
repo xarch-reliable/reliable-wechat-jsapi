35c7bf09390e68fd96b2dbcbe4de29228af4c670
wancy <wancy@wancy.com>
2019-09-29 22:02:44 +0800

    业务消息中间件测试

--- reliable-clear-center/src/main/java/org/xarch/reliable/service/ClearReceiver.java
+++ reliable-clear-center/src/main/java/org/xarch/reliable/service/ClearReceiver.java
@@ -1,10 +1,15 @@
 package org.xarch.reliable.service;
 
 import org.xarch.reliable.config.RabbitConfig;
+import org.xarch.reliable.utils.BaseResultTools;
+
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +18,15 @@ public class ClearReceiver {
 
 	private static final Logger logger = LoggerFactory.getLogger(ClearReceiver.class);
 	
+	@Autowired
+	private ClearServer clearServer;
+	
 	@RabbitHandler
-	public void receiveMessage(String message) {
-		logger.info("ClearReceiver[] 消息 <" + message + ">");
+	public void receiveMessage(String datastr) {
+		logger.info("ClearReceiver 接受消息 : " + datastr);
+		Map<String, Object> data = BaseResultTools.ObjectToMap(datastr);
+		clearServer.parseClearData(data);
+		return;
 	}
 	
 }
