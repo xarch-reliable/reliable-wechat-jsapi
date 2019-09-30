f2d7823ce62edc6dd9f0389d61d5857a9a8ea6b6
wancy <wancy@wancy.com>
2019-09-30 11:12:56 +0800

    添加日志

--- reliable-wechat-pay/src/main/java/org/xarch/reliable/service/PayMsgReceiver.java
+++ reliable-wechat-pay/src/main/java/org/xarch/reliable/service/PayMsgReceiver.java
@@ -5,6 +5,8 @@ import org.xarch.reliable.utils.BaseResultTools;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Service;
 
 public class PayMsgReceiver {
 	
+	private static final Logger logger = LoggerFactory.getLogger(PayMsgReceiver.class);
+	
 	@Autowired
 	private WxPayService wxPayService;
 	
@@ -21,6 +25,7 @@ public class PayMsgReceiver {
 	@RabbitHandler
 	@RabbitListener(queues = RabbitConfig.refundQueue)
 	public void RefundReceive(String datastr) {
+		logger.info("RefundReceive 接收消息 : " +datastr);
 		Map data = BaseResultTools.fromJSON(datastr, Map.class);
 		String outRefundNo = (String)((Map<String, Object>)data).get("out_refund_no");
 		String outTradeNo = (String)((Map<String, Object>)data).get("out_trade_no");
@@ -32,6 +37,7 @@ public class PayMsgReceiver {
 	@RabbitHandler
 	@RabbitListener(queues = RabbitConfig.payToUserQueue)
 	public void PayToUserReceive(String datastr) {
+		logger.info("PayToUserReceive 接收消息 : " +datastr);
 		Map data = BaseResultTools.fromJSON(datastr, Map.class);
 		String openid = (String)((Map<String, Object>)data).get("openid");
 		String partnerTradeNo = (String)((Map<String, Object>)data).get("partner_trade_no");
